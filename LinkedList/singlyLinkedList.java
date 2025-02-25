ae5dc20baff63c41878e5b55131c6995c02a71e2
Srinath U <srinathu73@gmail.com>
2025-02-25 11:11:56 +0530

    Added singlyLinkedList.java

--- LinkedList/singlyLinkedList.java
+++ LinkedList/singlyLinkedList.java
@@ -16,9 +16,61 @@ public class singlyLinkedList{
             System.out.print(current.data+" -> ");
             current = current.next;
         }
-        System.out.print("null");
+        System.out.println("null");
     }
 
+    public int length(){
+        if(head == null){
+            return 0;
+        }
+        Node current = head;
+        int count = 0;
+        while(current != null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    /* Insert a Node in the beginning of the Linked List */
+    public void insertFirst(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    /* Insert a Node at the end of the Linked List */
+    public void insertEnd(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    /* Insert a Node in a linked list at the given position */
+    public void insertAt(int data, int position){
+        Node newNode = new Node(data);
+        if(position == 1){
+            newNode.next = head;
+            head = newNode;
+        }else{
+            Node previous = head;
+            int count = 1;
+            while(count < position -1){
+                previous = previous.next;
+                count++;
+            }
+            Node current = previous.next;
+            newNode.next = current;
+            previous.next = newNode;
+        }
+    }
     public static void main(String[] args) {
         singlyLinkedList sll = new singlyLinkedList();
         sll.head = new Node(10);
@@ -27,6 +79,18 @@ public class singlyLinkedList{
         sll.head.next = second;
         second.next = third;
         third.next = null;
+        sll.display();
+        System.out.println("Lenght is: "+sll.length()) ;
+        //Node insertFirst = new Node(2);
+        sll.insertFirst(2);
+        sll.display();
+        System.out.println("Lenght is: "+sll.length()) ;
+        sll.insertEnd(20);
+        sll.display();
+        System.out.println("Lenght is: "+sll.length()) ;
+        sll.insertAt(35, 3);
+        sll.display();
+        sll.insertAt(9, 1);
         sll.display();
     }
 }
